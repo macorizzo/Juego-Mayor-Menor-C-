22b11e0147f931ac0e7b894d44b38e414640ef85
Martin <Martin@DESKTOP-3KJVNNK>
2021-09-01 21:01:37 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Juego_Mayor_Menor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numActual = 0;
+            int numSig;
+            String signo;
+            Random numRandom = new Random();
+            Console.WriteLine("El numero actual es: " + numActual);
+            bool resul = false;
+            int contador = 0;
+            while (resul == false)
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese '+' si considera que el siguiente sera mayor o '-' si considera que sera menor");
+                    signo = Console.ReadLine();
+                } while (!signo.Equals("+") && !signo.Equals("-"));
+
+                numSig = numRandom.Next(-10, 11);
+                Console.WriteLine("El que salio es: " + numSig);
+
+                if ((signo.Equals("+") && numSig >= numActual) || (signo.Equals("-") && numSig <= numActual)) 
+                {
+                    Console.WriteLine("Acertaste!, sigue el juego");
+                    numActual = numSig;
+                    contador++;
+                } else
+                {
+                    Console.WriteLine("Perdiste! Fin del juego");
+                    Console.WriteLine("Acertaste " + contador + " veces!.");
+                    resul = true;
+                }
+            }
+            Console.WriteLine("Presione cualquier tecla para salir...");
+            Console.ReadKey();
+            
+        }
+    }
+}
